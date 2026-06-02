0e6f10ca28e8dd9e6052d6ae9d599d05499c0b9c
Jeremy <jeldred1@byu.edu>
2026-06-02 09:37:20 -0600

    started work

---
+++ cubs_data_analysis.py
@@ -0,0 +1,20 @@
+"""
+Jeremy Eldredge
+IS 303 - A06
+
+Chicago Cubs Data Anlysis
+Analyzes data from the current 2026 MLB season to find star players.
+
+Inputs:
+- cubs_hitting_data.csv: 22 rows, columns: Rk,Player,Age,Pos,WAR,G,PA,AB,R,H,2B,3B,HR,RBI,SB,CS,BB,SO,
+    BA,OBP,SLG,OPS,OPS+,rOBA,Rbat+,TB,GIDP,HBP,SH,SF,IBB,Pos,Awards,Player-additional
+- cubs_pitching_data.csv: 28 rows, columns: Rk,Player,Age,Pos,WAR,W,L,W-L%,ERA,G,GS,GF,CG,SHO,SV,IP,H,
+    R,ER,HR,BB,IBB,SO,HBP,BK,WP,BF,ERA+,FIP,WHIP,H9,HR9,BB9,SO9,SO/BB,Awards,Player-additional
+
+Processes:
+- load_data(): reads CSV into DataFrame
+- clean_data(): convert numbers from strings, clean player names
+- validate_data(): assert no negative stats, verifies all values not null
+- analyze_data(): avg age of pitchers vs hitters (both), BA by defensive position (hitters), HR by salary (hitters), avg ERA and SO rate of starters vs releivers (pitchers), 
+    
+"""
