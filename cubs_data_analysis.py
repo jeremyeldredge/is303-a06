0bf76969ff638c4e5fb8515ab898665ed3cc2e6b
Jeremy <jeldred1@byu.edu>
2026-06-04 09:22:25 -0600

    started data validation

--- cubs_data_analysis.py
+++ cubs_data_analysis.py
@@ -6,6 +6,7 @@ Chicago Cubs Data Anlysis
 Analyzes data from the current 2026 MLB season to find star players.
 
 Inputs:
+- csv data from baseball-reference.com
 - cubs_hitting_data.csv: 22 rows, columns: Rk,Player,Age,Pos,WAR,G,PA,AB,R,H,2B,3B,HR,RBI,SB,CS,BB,SO,
     BA,OBP,SLG,OPS,OPS+,rOBA,Rbat+,TB,GIDP,HBP,SH,SF,IBB,Pos,Awards,Player-additional
 - cubs_pitching_data.csv: 28 rows, columns: Rk,Player,Age,Pos,WAR,W,L,W-L%,ERA,G,GS,GF,CG,SHO,SV,IP,H,
@@ -15,14 +16,14 @@ Processes:
 - load_data(): reads CSV into DataFrame
 - clean_data(): delete pitchers from hittig data, clean player names
 - validate_data(): assert no negative stats, verifies all values not null
-- analyze_data(): avg age of pitchers vs hitters (both), BA by defensive position (hitters), HR by salary (hitters), avg ERA and SO rate of starters vs releivers (pitchers)
+- analyze_data(): avg age of pitchers vs hitters (both), BA by defensive position (hitters), HR by salary (hitters), avg ERA and SO/BB rate of starters vs releivers (pitchers)
 - create_chart(): bar chart of HR by salary
 
 Outputs:
 - Avg age of pitchers and hitters (printed table)
 - Batting average per defensive position (printed table)
 - Bar chart of HR by salary (HR/$) saved as HR_by_salary.png
-- ERA and SO rate starters vs releivers (printed table)
+- ERA and SO/BB rate starters vs releivers (printed table)
 """
 
 import pandas as pd 
@@ -40,18 +41,46 @@ def load_pitching_data(filepath):
     print(f"Loaded {len(df_pitching)} rows from {filepath}")
     return df_pitching
 
-load_hitting_data("cubs_hitting_data.csv")
-load_pitching_data("cubs_pitching_data.csv")
+df_hitting = load_hitting_data("cubs_hitting_data.csv")
+df_pitching = load_pitching_data("cubs_pitching_data.csv")
 
 # clean data: remove pitchers from hitting data, remove symbols from names
 def clean_hitter_data(df):
 
     # remove rows of players with no batting average (pitchers)
-    df_hitting =df_hitting.dropna(subset=["BA"])
+    df = df.dropna(subset=["BA"])
 
     # remove (#) or (*) from end of player names
-    df_hitting["Player"]=df_hitting["Player"].str.rstrip("*#")
+    df["Player"] = df["Player"].str.rstrip("*#")
+
+    # remove awards and Player-additional columns
+    df = df.drop(columns=['Awards','Player-additional'])
+
+    print(f"Cleaned {len(df)} rows from cubs_hitting_data.csv")
+    return df
 
 def clean_pitcher_data(df):
     # remove (#) or (*) from end of player names
-    df_pitching["Player"]=df_pitching["Player"].str.rstrip("*#")
+    df["Player"] = df["Player"].str.rstrip("*#")
+
+    # remove pitchers with null SO/BB values
+    df = df.dropna(subset=["SO/BB"])
+
+    # remove awards and Player-additional columns
+    df = df.drop(columns=['Awards','Player-additional'])
+
+    print(f"Cleaned {len(df)} rows from cubs_pitching_data.csv")
+    return df
+
+df_hitting = clean_hitter_data(df_hitting)
+df_pitching = clean_pitcher_data(df_pitching)
+
+def validate_data(df):
+    assert df["BA"].notna().all(), "BA still has missing values"
+    assert df["SO/BB"].notna().all(), "SO/BB still has missing values"
+    assert len(df) > 0, "DataFrame is empty after cleaning"
+
+def analyze_hitting_data(df):
+    
+    hitter_avg_age = df[df['Age'].mean()]
+    return f"Average age of hitters: {hitter_avg_age:.1f}"
